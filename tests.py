df304a436373e5ed0315621342c6589ad609a176
David Durost <david.durost@gmail.com>
2025-11-19 19:43:20 -0500

    Verify course string/requirement relationships in tests

--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from core.tests import BaseDomainTestCase
 
-# Create your tests here.
+
+class CourseModelTests(BaseDomainTestCase):
+    def test_course_string_representation(self):
+        self.assertEqual(str(self.course), "Navigation Basics")
+
+    def test_course_requirement_relationship(self):
+        self.assertIn(self.requirement, self.course.requirements.all())
